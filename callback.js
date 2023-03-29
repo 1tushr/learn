8da91068c65df09ba3774659ddc40d3d25ec3cbc
Tushar Verma <tuhsar2022@gmail.com>
2023-03-29 19:38:25 +0530

    hello

--- callback.js
+++ callback.js
@@ -1,5 +1,12 @@
 setTimeout(function sayHello(){
 console.log('hello');
+setTimeout(function sayHi(){
+console.log("I am executing after 2 sec ")
+setTimeout (function (){
+console.log("i am another one ")
+},1500)
+
+},2000)
 },3000)
 
 console.log("hi");

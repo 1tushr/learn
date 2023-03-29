54736f9f725157854de74488aa1b141f0d3b2361
Tushar Verma <tuhsar2022@gmail.com>
2023-03-29 20:45:38 +0530

    made changes in object

--- callback.js
+++ callback.js
@@ -12,19 +12,7 @@ console.log("i am another one ")
 console.log("hi");
 
 
-const ages = [32, 33, 16, 40];
-const result = ages.filter(checkAdult);
-console.log(result);
-function checkAdult(age) {
-  return age >= 18;
-}
 
 
 
-var book ={
-    title:"secret",
-    author:{name:"tushar",
-age:23}
-}
 
-console.log(book.author.age)
--- objects.js
+++ objects.js
@@ -8,6 +8,17 @@ console.log();
 console.log(th);
 
 
+var book ={
+    title:"secret",
+    author:{name:"tushar",
+age:23}
+}
 
+console.log(book.author.age)
 
-
+const ages = [32, 33, 16, 40];
+const result = ages.filter(checkAdult);
+console.log(result);
+function checkAdult(age) {
+  return age >= 18;
+}

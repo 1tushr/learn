6adab03ac07f8c6106a896d45231c41d47c1b0bf
Tushar Verma <tuhsar2022@gmail.com>
2023-03-29 12:35:59 +0530

    first commit

---
+++ objects.js
@@ -0,0 +1,10 @@
+let gh ={name:'tushar',surmname:'verma'}
+// console.log(gh);
+let th=Object.entries(gh);
+th.forEach(element => {
+    console.log(element[0],element[1]);
+});
+console.log()
+console.log(th);
+
+
---
+++ script.js
@@ -0,0 +1,17 @@
+// function a (){
+//     console.log(b);
+// }
+// a();
+// var b=1;
+
+
+// {
+//     var a=10;
+//     var b=20;
+//     var c=30;
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+    
+// }
+// console.log(b);
